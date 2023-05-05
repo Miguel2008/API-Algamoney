f97f146ee549d40b67b4259bcf466accc6eeb60b
Miguel2008 <miguel_dasilva90@hotmail.com>
2023-05-05 17:36:11 -0300

    API-ALGAMONEY - PRONTO PARA PRODUÇÃO
    Signed-off-by: Miguel2008 <miguel_dasilva90@hotmail.com>

---
+++ src/main/java/com/algaworks/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -0,0 +1,16 @@
+package com.algaworks.algamoneyapi.repository.lancamento;
+
+import com.algaworks.algamoneyapi.model.Lancamento;
+import com.algaworks.algamoneyapi.repository.filter.LancamentoFilter;
+import com.algaworks.algamoneyapi.repository.projection.ResumoLancamento;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface LancamentoRepositoryQuery {
+
+    public Page<Lancamento> filtrar (LancamentoFilter lancamentoFilter, Pageable pageable);
+
+    public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable);
+}
